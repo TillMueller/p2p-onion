8571772dbfb679181e8bced071e6331cd35dec27
Valentin Langer <valentin.langer@tum.de>
2020-08-19 17:14:23 +0200

    Increase logging timeout for multiinstance tests to 10s

--- implementation/src/hoplayer/run_multiinstance_tests.sh
+++ implementation/src/hoplayer/run_multiinstance_tests.sh
@@ -8,7 +8,7 @@ go test network_peer2_test.go HopLayer.go -run TestDiffieHellmanExchangePeer2 &
 go test network_peer1_test.go HopLayer.go -run TestDiffieHellmanExchangePeer1 &
 #go test -v network_peer2_test.go HopLayer.go -run TestResetPeer2 &
 #go test -v network_peer1_test.go HopLayer.go -run TestResetPeer1 &
-timeout 5s tail -f onion.log
+timeout 10s tail -f onion.log
 for f in *_peer{1,2}_*; do
 	mv $f $(echo $f | sed -e 's/test/skiptest/')
 done
